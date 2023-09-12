b44e4f48aad3fb15b92874a8e059ff89ac315d59
data-asepitgans <133214500+data-asepitgans@users.noreply.github.com>
2023-09-12 12:17:37 +0700

    Update block.js

--- plugins/owner/block.js
+++ plugins/owner/block.js
@@ -1,4 +1,4 @@
-exports.run = {
+/*exports.run = {
    usage: ['block', 'unblock'],
    use: 'mention or reply',
    category: 'owner',
@@ -31,4 +31,4 @@ exports.run = {
       }
    },
    owner: true
-}
+}*/
