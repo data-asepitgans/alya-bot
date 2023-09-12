8719e7c0639c17e78d65f9fbfb395d06fec57eca
data-asepitgans <133214500+data-asepitgans@users.noreply.github.com>
2023-09-12 11:25:38 +0700

    Create informasi.js

---
+++ plugins/misc/informasi.js
@@ -0,0 +1,16 @@
+exports.run = {
+   usage: ['info'],
+   category: 'user info',
+   async: async (m, {
+      client,
+      isPrefix
+   }) => {
+     client.sendMessageModify(m.chat, `💀Bot ini dibuat oleh by AsepitgansXc  Store dan menggunakan apikey\n- alya\n- neo\n terimakasih.\n\nJika ingin bertanya hubungi ketik: *${isPrefix}owner*`, m, {
+       largeThumb: true,
+       thumbnail: 'https://telegra.ph/file/2050ce9e32b36da6f8db4.jpg'
+     })
+   },
+   error: false,
+   cache: true,
+   location: __filename
+}
